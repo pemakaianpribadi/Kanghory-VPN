9e2026f7af56b478be49ffb1410a250fcc942f04
kanghory <132531297+kanghory@users.noreply.github.com>
2023-05-27 19:45:14 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -87,5 +87,5 @@ systemctl restart xray
 
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "${YELLOW}          Terimakasih sudah menggunakan         "
-echo -e "                 Script Premium kanghoryVPN"
+echo -e "                    Script Premium kanghoryVPN"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
