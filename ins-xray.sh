fb74c7cee29f4a2108793240846d6c3329c9e6bf
kanghory <132531297+kanghory@users.noreply.github.com>
2023-06-04 02:43:09 +0700

    Update ins-xray.sh

--- ins-xray.sh
+++ ins-xray.sh
@@ -130,13 +130,13 @@ apt install -y nginx
 cd
 rm -fr /etc/nginx/sites-enabled/default
 rm -fr /etc/nginx/sites-available/default
-wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/nginx.conf.txt" 
+wget -q -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/kanghory/VPN/main/nginx.conf.txt" 
 mkdir -p /home/vps/public_html
-wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/vps.conf.txt"
+wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/kanghory/VPN/main/vps.conf.txt"
 
-wget -q -O xraymode.sh https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/Xray%20mode/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
+wget -q -O xraymode.sh https://raw.githubusercontent.com/kanghory/VPN/main/Xray%20mode/xraymode.sh && chmod +x xraymode.sh && ./xraymode.sh
 sleep 1 
-wget -q -O xray.conf https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/xray.conf.txt && chmod +x xray.conf && ./xray.conf
+wget -q -O xray.conf https://raw.githubusercontent.com/kanghory/VPN/main/xray.conf.txt && chmod +x xray.conf && ./xray.conf
 sleep 1 
 
 # Installing Xray Service
