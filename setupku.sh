329ce91267153205289e4a2cce6769444b2d94f3
kanghory <132531297+kanghory@users.noreply.github.com>
2023-06-18 11:20:40 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -90,7 +90,7 @@ echo -e "$green      Install SSH / WS               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/kanghory/VPN-3/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/kanghoryVPN/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/ssh-vpnjos.sh && chmod +x ssh-vpnjos.sh && ./ssh-vpnjos.sh
 sleep 2
 clear
 wget https://raw.githubusercontent.com/kanghory/VPN/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
