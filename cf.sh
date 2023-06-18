eb7a6cc459a72a74efd5eb058cd7332dfa31ef5c
kanghory <132531297+kanghory@users.noreply.github.com>
2023-06-18 15:32:02 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -5,10 +5,10 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 apt install jq curl -y
 sub=$(</dev/urandom tr -dc a-z | head -c4)
-DOMAIN=klmpk.my.id
-SUB_DOMAIN=${sub}.klmpk.my.id
-CF_ID=andyyuda41@gmail.com
-CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+DOMAIN=vvip-kanghory.my.id
+SUB_DOMAIN=${sub}.vvip-kanghory.my.id
+CF_ID=bukhorimukhammad@gmail.com
+CF_KEY=bd06fd9e8a01b73d24db51c4c6584d9133b3e
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
